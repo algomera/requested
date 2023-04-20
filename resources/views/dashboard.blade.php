90e17dccec80c3b9a00fc4312377a0c9d9446736
fabioserembe <fabio.serembe@gmail.com>
2023-04-20 10:40:23 +0200

    - Aggiunto application-logo temporaneo; - Fix ai loghi in layout;

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,5 @@
 <x-app-layout>
-{{--    <x-slot name="header">--}}
-{{--        <h2 class="font-semibold text-xl text-gray-800 leading-tight">--}}
-{{--            {{ __('Dashboard') }}--}}
-{{--        </h2>--}}
-{{--    </x-slot>--}}
-
-    <div>
-        Dashboard
-    </div>
+	<div>
+		Dashboard
+	</div>
 </x-app-layout>
