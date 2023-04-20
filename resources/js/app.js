6226b3e067af01afdd7f12238ed516f5cc9390ce
fabioserembe <fabio.serembe@gmail.com>
2023-04-20 10:33:26 +0200

    - Installazione plugin alpinejs/focus; - Fix grafici vari riguardanti il menù mobile; - Altri fix al layout;

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,10 +4,12 @@ import Alpine from 'alpinejs';
 import collapse from '@alpinejs/collapse'
 import mask from '@alpinejs/mask'
 import persist from '@alpinejs/persist'
+import focus from '@alpinejs/focus'
 
 Alpine.plugin(collapse)
 Alpine.plugin(mask)
 Alpine.plugin(persist)
+Alpine.plugin(focus)
 
 import tippy from "tippy.js";
 import 'tippy.js/dist/tippy.css';
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,8 +17,8 @@
 		@stack('styles')
 		@livewireStyles
 	</head>
-	<body class="font-sans antialiased">
-			{{--            @include('layouts.navigation')--}}
+	<body x-data="{sidebarOpen: false}" class="font-sans antialiased">
+		{{--            @include('layouts.navigation')--}}
 		<div class="lg:ml-72 xl:ml-80">
 			<header class="contents lg:pointer-events-none lg:fixed lg:inset-0 lg:z-40 lg:flex">
 				<div class="contents lg:pointer-events-auto lg:block lg:w-72 lg:overflow-y-auto lg:border-r lg:border-zinc-900/10 lg:px-6 lg:pb-8 lg:pt-4 xl:w-80">
@@ -47,13 +47,13 @@
 											class="font-sans">⌘</kbd><kbd class="font-sans">K</kbd></kbd></button>
 						</div>
 						<div class="flex items-center gap-5 lg:hidden">
-							<button type="button"
+							<button x-on:click="sidebarOpen = !sidebarOpen" type="button"
 							        class="flex h-6 w-6 items-center justify-center rounded-md transition hover:bg-zinc-900/5"
 							        aria-label="Toggle navigation">
-								<svg viewBox="0 0 10 9" fill="none" stroke-linecap="round" aria-hidden="true"
-								     class="w-2.5 stroke-zinc-900">
-									<path d="M.5 1h9M.5 8h9M.5 4.5h9"></path>
-								</svg>
+								<x-heroicon-o-bars-3 x-cloak x-show="!sidebarOpen"
+								                     class="w-4 stroke-zinc-900"></x-heroicon-o-bars-3>
+								<x-heroicon-o-x-mark x-cloak x-show="sidebarOpen"
+								                     class="w-4 stroke-zinc-900"></x-heroicon-o-x-mark>
 							</button>
 							<a aria-label="Home" href="/">
 								<svg viewBox="0 0 99 24" aria-hidden="true" class="h-6">
@@ -165,6 +165,66 @@
 							</li>
 						</ul>
 					</nav>
+					{{-- MOBILE --}}
+					<div x-cloak x-show="sidebarOpen"
+					     class="fixed inset-0 bg-zinc-900/5 top-14 z-10 backdrop-blur-sm lg:hidden"></div>
+					<div x-cloak x-show="sidebarOpen"
+					     class="fixed top-14 bg-white px-6 pt-4 pb-8 z-20 h-full shadow-lg shadow-zinc-900/10 w-80 overflow-y-scroll lg:hidden">
+						<ul role="list">
+							{{-- MENU ITEMS: HIDDEN DESKTOP/VISIBLE MOBILE --}}
+							<li class="md:hidden">
+								<a class="block py-1 text-sm text-zinc-600 transition hover:text-zinc-900"
+								   href="/">
+									API
+								</a>
+							</li>
+							<li class="md:hidden">
+								<a class="block py-1 text-sm text-zinc-600 transition hover:text-zinc-900"
+								   href="#">
+									Documentation
+								</a>
+							</li>
+							{{-- MENU ITEMS --}}
+							<li class="relative mt-6 md:mt-0">
+								<h2 class="text-xs font-semibold text-zinc-900">Guides</h2>
+								<div class="relative mt-3 pl-2">
+									<div class="absolute inset-y-0 left-2 w-px bg-zinc-900/10"></div>
+									<div class="absolute left-2 top-1 h-6 w-px bg-emerald-500"></div>
+									<ul role="list" class="border-l border-transparent">
+										<li class="relative">
+											{{-- MAIN ITEM --}}
+											<a aria-current="page"
+											   class="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4 text-zinc-900"
+											   href="/">
+												<span class="truncate">Introduction</span>
+											</a>
+											{{-- SUB ITEMS --}}
+											<ul role="list" style="opacity:1">
+												<li>
+													<a class="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-7 text-zinc-600 hover:text-zinc-900"
+													   href="#">
+														<span class="truncate">Guides</span>
+													</a>
+												</li>
+												<li>
+													<a class="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-7 text-zinc-600 hover:text-zinc-900"
+													   href="#">
+														<span class="truncate">Resources</span>
+													</a>
+												</li>
+											</ul>
+										</li>
+									</ul>
+								</div>
+							</li>
+							<li class="sticky bottom-0 z-10 mt-6 min-[416px]:hidden">
+								<a class="inline-flex gap-0.5 justify-center overflow-hidden text-sm font-medium transition rounded-full bg-zinc-900 py-1 px-3 text-white hover:bg-zinc-700 w-full"
+								   href="#">
+									Sign in
+								</a>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</header>
 
