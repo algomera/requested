ed38c0ed63c878ac4404fe67e89d8672acc59089
fabioserembe <fabio.serembe@gmail.com>
2023-04-20 10:55:38 +0200

    - Aggiunte voci fake al componente navigation.blade.php da utilizzare come menù sidebar (desktop/mobile); - Fix delle routes;

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,6 @@
 		@livewireStyles
 	</head>
 	<body x-data="{sidebarOpen: false}" class="font-sans antialiased">
-		{{--            @include('layouts.navigation')--}}
 		<div class="lg:ml-72 xl:ml-80">
 			<header class="contents lg:pointer-events-none lg:fixed lg:inset-0 lg:z-40 lg:flex">
 				<div class="contents lg:pointer-events-auto lg:block lg:w-72 lg:overflow-y-auto lg:border-r lg:border-zinc-900/10 lg:px-6 lg:pb-8 lg:pt-4 xl:w-80">
@@ -101,120 +100,14 @@
 					</div>
 					{{-- NAVIGATION MENU --}}
 					<nav class="hidden lg:mt-8 lg:block">
-						<ul role="list">
-							{{-- MENU ITEMS: HIDDEN DESKTOP/VISIBLE MOBILE --}}
-							<li class="md:hidden">
-								<a class="block py-1 text-sm text-zinc-600 transition hover:text-zinc-900"
-								   href="/">
-									API
-								</a>
-							</li>
-							<li class="md:hidden">
-								<a class="block py-1 text-sm text-zinc-600 transition hover:text-zinc-900"
-								   href="#">
-									Documentation
-								</a>
-							</li>
-							{{-- MENU ITEMS --}}
-							<li class="relative mt-6 md:mt-0">
-								<h2 class="text-xs font-semibold text-zinc-900">Guides</h2>
-								<div class="relative mt-3 pl-2">
-									<div class="absolute inset-y-0 left-2 w-px bg-zinc-900/10"></div>
-									<div class="absolute left-2 top-1 h-6 w-px bg-emerald-500"></div>
-									<ul role="list" class="border-l border-transparent">
-										<li class="relative">
-											{{-- MAIN ITEM --}}
-											<a aria-current="page"
-											   class="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4 text-zinc-900"
-											   href="/">
-												<span class="truncate">Introduction</span>
-											</a>
-											{{-- SUB ITEMS --}}
-											<ul role="list" style="opacity:1">
-												<li>
-													<a class="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-7 text-zinc-600 hover:text-zinc-900"
-													   href="#">
-														<span class="truncate">Guides</span>
-													</a>
-												</li>
-												<li>
-													<a class="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-7 text-zinc-600 hover:text-zinc-900"
-													   href="#">
-														<span class="truncate">Resources</span>
-													</a>
-												</li>
-											</ul>
-										</li>
-									</ul>
-								</div>
-							</li>
-							<li class="sticky bottom-0 z-10 mt-6 min-[416px]:hidden">
-								<a class="inline-flex gap-0.5 justify-center overflow-hidden text-sm font-medium transition rounded-full bg-zinc-900 py-1 px-3 text-white hover:bg-zinc-700 w-full"
-								   href="#">
-									Sign in
-								</a>
-							</li>
-						</ul>
+						@include('layouts.navigation')
 					</nav>
 					{{-- MOBILE --}}
 					<div x-cloak x-show="sidebarOpen"
 					     class="fixed inset-0 bg-zinc-900/5 top-14 z-10 backdrop-blur-sm lg:hidden"></div>
 					<div x-cloak x-show="sidebarOpen"
 					     class="fixed top-14 bg-white px-6 pt-4 pb-8 z-20 h-full shadow-lg shadow-zinc-900/10 w-80 overflow-y-scroll lg:hidden">
-						<ul role="list">
-							{{-- MENU ITEMS: HIDDEN DESKTOP/VISIBLE MOBILE --}}
-							<li class="md:hidden">
-								<a class="block py-1 text-sm text-zinc-600 transition hover:text-zinc-900"
-								   href="/">
-									API
-								</a>
-							</li>
-							<li class="md:hidden">
-								<a class="block py-1 text-sm text-zinc-600 transition hover:text-zinc-900"
-								   href="#">
-									Documentation
-								</a>
-							</li>
-							{{-- MENU ITEMS --}}
-							<li class="relative mt-6 md:mt-0">
-								<h2 class="text-xs font-semibold text-zinc-900">Guides</h2>
-								<div class="relative mt-3 pl-2">
-									<div class="absolute inset-y-0 left-2 w-px bg-zinc-900/10"></div>
-									<div class="absolute left-2 top-1 h-6 w-px bg-emerald-500"></div>
-									<ul role="list" class="border-l border-transparent">
-										<li class="relative">
-											{{-- MAIN ITEM --}}
-											<a aria-current="page"
-											   class="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4 text-zinc-900"
-											   href="/">
-												<span class="truncate">Introduction</span>
-											</a>
-											{{-- SUB ITEMS --}}
-											<ul role="list" style="opacity:1">
-												<li>
-													<a class="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-7 text-zinc-600 hover:text-zinc-900"
-													   href="#">
-														<span class="truncate">Guides</span>
-													</a>
-												</li>
-												<li>
-													<a class="flex justify-between gap-2 py-1 pr-3 text-sm transition pl-7 text-zinc-600 hover:text-zinc-900"
-													   href="#">
-														<span class="truncate">Resources</span>
-													</a>
-												</li>
-											</ul>
-										</li>
-									</ul>
-								</div>
-							</li>
-							<li class="sticky bottom-0 z-10 mt-6 min-[416px]:hidden">
-								<a class="inline-flex gap-0.5 justify-center overflow-hidden text-sm font-medium transition rounded-full bg-zinc-900 py-1 px-3 text-white hover:bg-zinc-700 w-full"
-								   href="#">
-									Sign in
-								</a>
-							</li>
-						</ul>
+						@include('layouts.navigation')
 					</div>
 				</div>
 			</header>
--- routes/web.php
+++ routes/web.php
@@ -3,25 +3,19 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
+// Guest Routes
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Auth Routes
+Route::middleware(['auth', 'verified'])->group(function() {
+	Route::get('/dashboard', function () {
+		return view('dashboard');
+	})->name('dashboard');
+});
 
+// Profile Routes
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
