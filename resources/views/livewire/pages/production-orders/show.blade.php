435079c9480cb401f99450b1059778936123c706
fabioserembe <fabio.serembe@gmail.com>
2023-05-05 17:22:58 +0200

    - Aggiunta dati base (no grafica) alla pagina production-orders.show;

--- resources/views/livewire/pages/production-orders/show.blade.php
+++ resources/views/livewire/pages/production-orders/show.blade.php
@@ -1,3 +1,21 @@
-<div>
-    Production Order: {{ $production_order->code }}
+<div class="space-y-3">
+	<p>Production Order: {{ $production_order->code }}</p>
+	<hr>
+	<p>Articoli da produrre: {{ $production_order->quantity }} &times; {{ $production_order->item->product->name }}</p>
+	<hr>
+	<div class="space-y-2">
+		<p>Materiali da utilizzare:</p>
+		<div>
+			<p class="font-bold">Totale:</p>
+			@foreach($production_order->item->products as $product)
+				<p>{{ $product->pivot->quantity * $production_order->quantity }} &times; {{ $product->name }}</p>
+			@endforeach
+		</div>
+		<div>
+			<p class="font-bold">Singolo:</p>
+			@foreach($production_order->item->products as $product)
+				<p>{{ $product->pivot->quantity }} &times; {{ $product->name }}</p>
+			@endforeach
+		</div>
+	</div>
 </div>
