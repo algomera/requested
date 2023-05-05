67ecff77081d37242c4240550e7250e7a6ffd4e9
fabioserembe <fabio.serembe@gmail.com>
2023-05-05 12:54:06 +0200

    - Aggiunte voci in menu "Ordini di Produzione", "Destinazioni" e "Log di sistema";

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -43,15 +43,61 @@
 		</div>
 	</li>
 	<li>
-		<a class="{{ request()->is('users*') ? 'text-emerald-500 font-medium' : 'text-zinc-600 hover:text-zinc-900' }} block py-1 text-sm transition"
-		   href="{{ route('users.index') }}">
-			Utenti
+		<a class="{{ request()->is('order*') ? 'text-emerald-500 font-medium' : 'text-zinc-600 hover:text-zinc-900' }} block py-1 text-sm transition"
+		   href="#">
+			Ordini di Produzione
 		</a>
 	</li>
 	<li>
-		<a class="{{ request()->is('suppliers*') ? 'text-emerald-500 font-medium' : 'text-zinc-600 hover:text-zinc-900' }} block py-1 text-sm transition"
-		   href="{{ route('suppliers.index') }}">
-			Fornitori
-		</a>
+		<h2 class="text-xs font-semibold text-zinc-900">Anagrafiche</h2>
+		<div class="relative mt-3 pl-2">
+			<div class="absolute inset-y-0 left-2 w-px bg-zinc-900/10"></div>
+			<ul role="list" class="border-l border-transparent">
+				<li class="relative">
+					<a class="{{ request()->is('users*') ? 'text-emerald-500 font-medium' : 'text-zinc-600 hover:text-zinc-900' }} block py-1 text-sm transition pl-4"
+					   href="{{ route('users.index') }}">
+						@if(request()->is('users*'))
+							<div class="absolute -left-px top-1 h-6 w-px bg-emerald-500"></div>
+						@endif
+						<span class="truncate">Utenti</span>
+					</a>
+				</li>
+				<li class="relative">
+					<a class="{{ request()->is('suppliers*') ? 'text-emerald-500 font-medium' : 'text-zinc-600 hover:text-zinc-900' }} block py-1 text-sm transition pl-4"
+					   href="{{ route('suppliers.index') }}">
+						@if(request()->is('suppliers*'))
+							<div class="absolute -left-px top-1 h-6 w-px bg-emerald-500"></div>
+						@endif
+						<span class="truncate">Fornitori</span>
+					</a>
+				</li>
+				<li class="relative">
+					<a class="{{ request()->is('destinations*') ? 'text-emerald-500 font-medium' : 'text-zinc-600 hover:text-zinc-900' }} block py-1 text-sm transition pl-4"
+					   href="#">
+						@if(request()->is('destinations*'))
+							<div class="absolute -left-px top-1 h-6 w-px bg-emerald-500"></div>
+						@endif
+						<span class="truncate">Destinazioni</span>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</li>
+	<li>
+		<h2 class="text-xs font-semibold text-zinc-900">Altro</h2>
+		<div class="relative mt-3 pl-2">
+			<div class="absolute inset-y-0 left-2 w-px bg-zinc-900/10"></div>
+			<ul role="list" class="border-l border-transparent">
+				<li class="relative">
+					<a class="{{ request()->is('logs*') ? 'text-emerald-500 font-medium' : 'text-zinc-600 hover:text-zinc-900' }} block py-1 text-sm transition pl-4"
+					   href="#">
+						@if(request()->is('logs*'))
+							<div class="absolute -left-px top-1 h-6 w-px bg-emerald-500"></div>
+						@endif
+						<span class="truncate">Log di sistema</span>
+					</a>
+				</li>
+			</ul>
+		</div>
 	</li>
 </ul>
