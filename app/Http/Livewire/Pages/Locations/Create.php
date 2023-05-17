83416cb8cd2162ea9823afb8f6270b6d21b8173d
fabioserembe <fabio.serembe@gmail.com>
2023-05-17 17:09:48 +0200

    - Aggiunto log alla creazione di una location;

--- app/Http/Livewire/Pages/Locations/Create.php
+++ app/Http/Livewire/Pages/Locations/Create.php
@@ -9,30 +9,37 @@
 	{
 		public $code, $description, $type;
 
-		protected function rules() {
+		protected function rules()
+		{
 			return [
-				'code'        => 'required|unique:locations,code',
+				'code' => 'required|unique:locations,code',
 				'description' => 'required|unique:locations,description',
-				'type'        => 'required|in:' . implode(',', array_keys(config('requested.locations.types'))),
+				'type' => 'required|in:' . implode(',', array_keys(config('requested.locations.types'))),
 			];
 		}
 
-		public function save() {
-			Location::create([
-				'code'        => $this->code,
+		public function save()
+		{
+			$location = Location::create([
+				'code' => $this->code,
 				'description' => $this->description,
-				'type'        => $this->type
+				'type' => $this->type
 			]);
 			$this->emitTo('pages.locations.index', 'location-created');
 			$this->closeModal();
+			$location->logs()->create([
+				'user_id' => auth()->id(),
+				'message' => "ha creato l'ubicazione '{$location->code}' (tipologia: " . config('requested.locations.types.' . $location->type) . ")"
+			]);
 			$this->dispatchBrowserEvent('open-notification', [
-				'title'    => __('Ubicazione Creata'),
+				'title' => __('Ubicazione Creata'),
 				'subtitle' => __('L\'ubicazione è stata creata con successo!'),
-				'type'     => 'success'
+				'type' => 'success'
 			]);
 		}
 
-		public function render() {
+		public function render()
+		{
 			return view('livewire.pages.locations.create');
 		}
 	}
