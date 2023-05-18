e875eb99fa5d9c22aad85a5fbef83a9a0e32c7ba
fabioserembe <fabio.serembe@gmail.com>
2023-05-18 09:24:10 +0200

    - Aggiunto effetto hover alle voci in menù;

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,30 +12,24 @@
 		<div class="relative mt-3 pl-2">
 			<div class="absolute inset-y-0 left-2 w-px bg-zinc-900/10"></div>
 			<ul role="list" class="border-l border-transparent">
-				<li class="relative">
+				<li class="relative group">
 					<a class="{{ request()->is('locations*') ? 'text-zinc-900 font-medium' : 'text-zinc-600 hover:text-zinc-900' }} flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4"
 					   href="{{ route('locations.index') }}">
-						@if(request()->is('locations*'))
-							<div class="absolute -left-px top-1 h-6 w-px bg-emerald-500"></div>
-						@endif
+						<div class="absolute -left-px top-1 h-6 w-px {{ request()->is('locations*') ? 'bg-emerald-500' : 'group-hover:bg-gray-300' }}"></div>
 						<span class="truncate">Ubicazioni</span>
 					</a>
 				</li>
-				<li class="relative">
+				<li class="relative group">
 					<a class="{{ request()->is('products*') ? 'text-zinc-900 font-medium' : 'text-zinc-600 hover:text-zinc-900' }} flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4"
 					   href="{{ route('products.index') }}">
-						@if(request()->is('products*'))
-							<div class="absolute -left-px top-1 h-6 w-px bg-emerald-500"></div>
-						@endif
+						<div class="absolute -left-px top-1 h-6 w-px {{ request()->is('products*') ? 'bg-emerald-500' : 'group-hover:bg-gray-300' }}"></div>
 						<span class="truncate">Anagrafica Prodotti</span>
 					</a>
 				</li>
-				<li class="relative">
+				<li class="relative group">
 					<a class="{{ request()->is('items*') ? 'text-zinc-900 font-medium' : 'text-zinc-600 hover:text-zinc-900' }} flex justify-between gap-2 py-1 pr-3 text-sm transition pl-4"
 					   href="{{ route('items.index') }}">
-						@if(request()->is('items*'))
-							<div class="absolute -left-px top-1 h-6 w-px bg-emerald-500"></div>
-						@endif
+						<div class="absolute -left-px top-1 h-6 w-px {{ request()->is('items*') ? 'bg-emerald-500' : 'group-hover:bg-gray-300' }}"></div>
 						<span class="truncate">Articoli</span>
 					</a>
 				</li>
@@ -53,30 +47,24 @@
 		<div class="relative mt-3 pl-2">
 			<div class="absolute inset-y-0 left-2 w-px bg-zinc-900/10"></div>
 			<ul role="list" class="border-l border-transparent">
-				<li class="relative">
+				<li class="relative group">
 					<a class="{{ request()->is('users*') ? 'text-emerald-500 font-medium' : 'text-zinc-600 hover:text-zinc-900' }} block py-1 text-sm transition pl-4"
 					   href="{{ route('users.index') }}">
-						@if(request()->is('users*'))
-							<div class="absolute -left-px top-1 h-6 w-px bg-emerald-500"></div>
-						@endif
+						<div class="absolute -left-px top-1 h-6 w-px {{ request()->is('users*') ? 'bg-emerald-500' : 'group-hover:bg-gray-300' }}"></div>
 						<span class="truncate">Utenti</span>
 					</a>
 				</li>
-				<li class="relative">
+				<li class="relative group">
 					<a class="{{ request()->is('suppliers*') ? 'text-emerald-500 font-medium' : 'text-zinc-600 hover:text-zinc-900' }} block py-1 text-sm transition pl-4"
 					   href="{{ route('suppliers.index') }}">
-						@if(request()->is('suppliers*'))
-							<div class="absolute -left-px top-1 h-6 w-px bg-emerald-500"></div>
-						@endif
+						<div class="absolute -left-px top-1 h-6 w-px {{ request()->is('suppliers*') ? 'bg-emerald-500' : 'group-hover:bg-gray-300' }}"></div>
 						<span class="truncate">Fornitori</span>
 					</a>
 				</li>
-				<li class="relative">
+				<li class="relative group">
 					<a class="{{ request()->is('destinations*') ? 'text-emerald-500 font-medium' : 'text-zinc-600 hover:text-zinc-900' }} block py-1 text-sm transition pl-4"
 					   href="{{ route('destinations.index') }}">
-						@if(request()->is('destinations*'))
-							<div class="absolute -left-px top-1 h-6 w-px bg-emerald-500"></div>
-						@endif
+						<div class="absolute -left-px top-1 h-6 w-px {{ request()->is('destinations*') ? 'bg-emerald-500' : 'group-hover:bg-gray-300' }}"></div>
 						<span class="truncate">Destinazioni</span>
 					</a>
 				</li>
@@ -88,12 +76,10 @@
 		<div class="relative mt-3 pl-2">
 			<div class="absolute inset-y-0 left-2 w-px bg-zinc-900/10"></div>
 			<ul role="list" class="border-l border-transparent">
-				<li class="relative">
+				<li class="relative group">
 					<a class="{{ request()->is('logs*') ? 'text-emerald-500 font-medium' : 'text-zinc-600 hover:text-zinc-900' }} block py-1 text-sm transition pl-4"
 					   href="{{ route('logs.index') }}">
-						@if(request()->is('logs*'))
-							<div class="absolute -left-px top-1 h-6 w-px bg-emerald-500"></div>
-						@endif
+						<div class="absolute -left-px top-1 h-6 w-px {{ request()->is('logs*') ? 'bg-emerald-500' : 'group-hover:bg-gray-300' }}"></div>
 						<span class="truncate">Log di sistema</span>
 					</a>
 				</li>
