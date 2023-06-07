3d522df90141b1ed0eb963f37545456094ff70e4
fabioserembe <fabio.serembe@gmail.com>
2023-06-07 12:27:28 +0200

    - Aggiunta lista "base" delle DDT; - Aggiunta icona "print" (che non fa nulla al momento);

--- resources/views/livewire/pages/warehouse-orders/show.blade.php
+++ resources/views/livewire/pages/warehouse-orders/show.blade.php
@@ -1,5 +1,6 @@
 <div class="{{ $warehouse_order->type === 'spedizione' ? 'grid grid-cols-1 2xl:grid-cols-3 gap-1' : '' }}">
-	<div class="flow-root space-y-5 {{ $warehouse_order->type === 'spedizione' ? 'col-span-2 2xl:pr-4 2xl:border-r' : '' }}">
+	<div
+		class="flow-root space-y-5 {{ $warehouse_order->type === 'spedizione' ? 'col-span-2 2xl:pr-4 2xl:border-r' : '' }}">
 		<div class="flex items-center space-x-3">
 			<div>
 				<div class="flex items-center space-x-4">
@@ -135,24 +136,24 @@
 			<h3 class="mb-2 text-sm text-center">DDTs</h3>
 			<ul role="list" class="space-y-6">
 				@forelse($ddts as $ddt)
-{{--					<li class="relative flex gap-x-4">--}}
-{{--						@if(!$loop->last)--}}
-{{--							<div class="absolute left-0 top-0 flex w-6 justify-center -bottom-6">--}}
-{{--								<div class="w-px bg-gray-200"></div>--}}
-{{--							</div>--}}
-{{--						@endif--}}
-{{--						<div class="relative flex h-6 w-6 flex-none items-center justify-center bg-white">--}}
-{{--							<div class="h-1.5 w-1.5 rounded-full bg-gray-100 ring-1 ring-gray-300"></div>--}}
-{{--						</div>--}}
-{{--						<p class="flex-auto py-0.5 text-xs leading-5 text-gray-500">--}}
-{{--							<span class="font-medium text-gray-900">{{ $log->user->fullName }}</span>--}}
-{{--							{{ $log->message }}--}}
-{{--						</p>--}}
-{{--						<span x-tooltip="{{ $log->created_at->format('d-m-Y H:i:s') }}"--}}
-{{--							  class="flex-none py-0.5 text-xs leading-5 text-gray-500">--}}
-{{--							{{ $log->created_at->diffForHumans() }}--}}
-{{--						</span>--}}
-{{--					</li>--}}
+					<li class="relative flex items-center gap-x-4">
+						@if(!$loop->last)
+							<div class="absolute left-0 top-0 flex w-6 justify-center -bottom-6">
+								<div class="w-px bg-gray-200"></div>
+							</div>
+						@endif
+						<div class="relative flex h-6 w-6 flex-none items-center justify-center bg-white">
+							<div class="h-1.5 w-1.5 rounded-full bg-gray-100 ring-1 ring-gray-300"></div>
+						</div>
+						<p class="flex-auto py-0.5 text-xs leading-5 text-gray-500">
+							<span class="font-medium text-gray-900">DDT: {{ $ddt->id }}</span>
+							<span x-tooltip="{{ $ddt->created_at->format('d-m-Y H:i:s') }}"
+								  class="block flex-none py-0.5 text-xs leading-5 text-gray-500">
+							{{ $ddt->created_at->diffForHumans() }}
+						</span>
+						</p>
+						<x-heroicon-s-printer class="w-4 h-4 hover:cursor-pointer hover:text-indigo-500"></x-heroicon-s-printer>
+					</li>
 				@empty
 					<p class="text-center mt-3 text-gray-500 text-xs">Al momento non c'è nessun DDT generato.</p>
 				@endforelse
