a30ba6d9567053ce1335eec9a0e28900e32628ee
fabioserembe <fabio.serembe@gmail.com>
2023-06-08 10:54:46 +0200

    - Fix locations in change.blade.php;

--- resources/views/livewire/pages/locations/change.blade.php
+++ resources/views/livewire/pages/locations/change.blade.php
@@ -16,7 +16,7 @@
 				<dt class="flex items-center justify-between text-sm font-medium text-gray-900">
 					<span>Destinazione</span>
 					<span wire:click="addLocation"
-					      class="text-xs font-medium text-indigo-500 hover:text-indigo-800 hover:cursor-pointer">Aggiungi</span>
+						  class="text-xs font-medium text-indigo-500 hover:text-indigo-800 hover:cursor-pointer">Aggiungi</span>
 				</dt>
 				<dd class="mt-1 text-sm leading-6 text-gray-700 sm:mt-0">
 					<ul role="list" class="divide-y divide-gray-200">
@@ -27,7 +27,10 @@
 										<x-select wire:model="locations.{{$k}}.id">
 											<option value="" selected>Seleziona</option>
 											@foreach($all_locations as $loc)
-												<option {{ in_array($loc->id, array_column($locations, 'id')) ? 'disabled' : '' }} value="{{ $loc->id }}">{{ $loc->code }}</option>
+												@if(config('requested.locations.types.' . $loc->type . '.user_can_show'))
+													<option
+														{{ in_array($loc->id, array_column($locations, 'id')) ? 'disabled' : '' }} value="{{ $loc->id }}">{{ $loc->code }}</option>
+												@endif
 											@endforeach
 										</x-select>
 									</div>
@@ -35,7 +38,7 @@
 										<x-input wire:model="locations.{{$k}}.quantity" type="number" step="1" min="1"/>
 									</div>
 									<div wire:click="removeLocation({{$k}})"
-									     class="p-2 rounded-md hover:bg-red-100 hover:cursor-pointer @error('locations.' . $k . '.quantity') transform -translate-y-[0.6rem] @enderror">
+										 class="p-2 rounded-md hover:bg-red-100 hover:cursor-pointer @error('locations.' . $k . '.quantity') transform -translate-y-[0.6rem] @enderror">
 										<x-heroicon-o-trash class="w-4 h-4 text-red-500"></x-heroicon-o-trash>
 									</div>
 								</div>
