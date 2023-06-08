4dd71680dbf767b77d488891e6fdf90d22423911
fabioserembe <fabio.serembe@gmail.com>
2023-06-08 10:27:20 +0200

    - Aggiunta colonna "generated_at" alla tabella "ddts";

--- database/migrations/2023_06_05_165351_create_ddts_table.php
+++ database/migrations/2023_06_05_165351_create_ddts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
 			$table->foreignIdFor(\App\Models\WarehouseOrder::class, 'warehouse_order_id');
 			$table->boolean('generated')->default(false);
+	        $table->timestamp('generated_at')->nullable();
             $table->timestamps();
         });
     }
